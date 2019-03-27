669a817c74518037445cb7eb3f55b91d7e2df40c
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-03-26 20:49:14 -0500

    Initial Commit

---
+++ Scribe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scribe
+//
+//  Created by Ricardo Herrera Petit on 3/26/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
